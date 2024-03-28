e121d1fb2d5ce70d679d32c804c8595495360d24
Chris <152150297+innmonkey@users.noreply.github.com>
2024-03-28 11:52:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,3 +11,53 @@
 #
 
 
+# 移除要替换的包
+#rm -rf feeds/packages/net/v2ray-geodata
+#rm -rf feeds/packages/net/mosdns
+#rm -rf feeds/luci/applications/luci-app-mosdns
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+# 添加额外插件
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-aliddns
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-jellyfin luci-lib-taskd luci-lib-xterm taskd
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-linkease linkease ffmpeg-remux
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# 加入OpenClash核心
+chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
+$GITHUB_WORKSPACE/preset-clash-core.sh
+
+echo "
+# openclash
+CONFIG_PACKAGE_luci-app-openclash=y
+
+# 阿里DDNS
+CONFIG_PACKAGE_luci-app-aliddns=y
+
+# filebrowser
+CONFIG_PACKAGE_luci-app-filebrowser=y
+
+# Jellyfin
+CONFIG_PACKAGE_luci-app-jellyfin=y
+
+# 易有云
+CONFIG_PACKAGE_luci-app-linkease=y
+
+" >> .config
+
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.0.2/g' package/base-files/files/bin/config_generate
+
+# 修改默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
